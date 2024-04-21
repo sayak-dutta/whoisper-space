1cafc0fb43c1ea995222b1a10cf360963bf6abae
Soumyadip Das <103370490+soumyadipdas14@users.noreply.github.com>
2024-04-21 09:06:01 +0000

    "coms"

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,11 +1,19 @@
 "use client";
-import { Typography } from 'antd'
+import { Image, Space, Typography } from 'antd'
 import React from 'react'
 
 export default function Header() {
     return (
         <div className="fixed-top text-center py-3">
-            <Typography.Title className='my-auto fs-5'>Who-is-per</Typography.Title>
+            <Space className="bg-dark px-3 pt-2 rounded-5">
+                <Image
+                    height={50}
+                    src={"./logo-alt.svg"}
+                    alt="logo"
+                    style={{ display: "flex", margin: "auto" }}
+                    className="logoHeader my-auto p-0 m-0"
+                />
+            </Space>
         </div>
     )
 }
