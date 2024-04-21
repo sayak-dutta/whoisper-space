447f5137d87ff63ca54973cd8a1f04e7bbd129a4
Soumyadip Das <103370490+soumyadipdas14@users.noreply.github.com>
2024-04-21 08:50:39 +0000

    "init"

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,11 @@
+"use client";
+import { Typography } from 'antd'
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className="fixed-top text-center py-3">
+            <Typography.Title className='my-auto fs-5'>Who-is-per</Typography.Title>
+        </div>
+    )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,21 @@
+'use client'
+import { Col, Image, Row } from "antd";
+export default function Home() {
+
+	return (
+		<>
+
+			<div className="position-absolute some top-0 w-100 h-100 border-danger overflow-auto">
+				<div className="mx-2">
+					<Row className="cloud-container position-relative mt-5" gutter={[10, 20]}>
+						{[...new Array(10)].map((i, iIndex) => (<Col key={iIndex} xs={24} md={6}>
+							<div className="text-center">
+								<Image preview={false} height={180} className="cloud" src="/cloud.png" />
+							</div>
+						</Col>))}
+					</Row>
+				</div>
+			</div>
+		</>
+	);
+}
